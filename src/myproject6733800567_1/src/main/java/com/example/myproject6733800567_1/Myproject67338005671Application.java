4cd906fa0f6f084ea96e402112cc6f5c4c9df2ee
Rapeephat-kp <rapeephat.k@kkumail.com>
2026-07-15 14:02:50 +0700

    Edit-HelloController

---
+++ src/myproject6733800567_1/src/main/java/com/example/myproject6733800567_1/Myproject67338005671Application.java
@@ -0,0 +1,13 @@
+package com.example.myproject6733800567_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myproject67338005671Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myproject67338005671Application.class, args);
+	}
+
+}
---
+++ src/myproject6733800567_1/src/test/java/com/example/myproject6733800567_1/Myproject67338005671ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproject6733800567_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myproject67338005671ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
