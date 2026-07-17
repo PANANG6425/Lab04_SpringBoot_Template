2b0b33bf79af0ab5fb6bd60d799dabcc0b936148
Kriangkrai Prasert <kriangkrai.p@kkumail.com>
2026-07-17 13:59:45 +0700

    Update HelloController.java

--- src/main/java/com/example/lab04/controller/HelloController.java
+++ src/main/java/com/example/lab04/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.lab03.controller;
+package com.example.lab04.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
